9eabc63493cc4f1b79aa0f1e38a9189605df3b23
ka1rakan <kairaskaraca@gmail.com>
2024-06-28 20:24:42 +1000

    Implement decimal point deletion

--- script.js
+++ script.js
@@ -30,16 +30,20 @@ function clickButton(button) {
         }
         currOp = button.id;
         floating = false;
-    } else if (button.id == 'del') {
+    } else if (button.id == "del") {
+        if (floating) {
+            floating = false;
+        }
         if (currOp == null) {
             num1 = Math.floor(num1 / 10);
         } else {
             num2 = Math.floor(num2 / 10);
         }
-    } else if (button.id == 'clear') {
+    } else if (button.id == "clear") {
         num1 = 0;
         num2 = 0;
         currOp = null;
+        floating = false;
     } else if (button.id == '=') {
         operate();
     } else if (button.id == '.') {
