5b5da97e40a556e743d1b22ef5a4c2a0af8678bc
ka1rakan <kairaskaraca@gmail.com>
2024-08-09 07:52:34 +1000

    Add multiple exponential floats

--- script.js
+++ script.js
@@ -4,37 +4,43 @@ buttons.forEach(button => button.addEventListener("click", (e) => clickButton(e.
 
 let num1 = 0;
 let num2 = 0;
-let floating = false;
+let floatExp = 0;
 let resultOnDisplay = 0;
 let currOp = null;
 function clickButton(button) {
     let isDot = false;
     if (button.id >= '0' && button.id <= '9') {
         if (currOp == null && !resultOnDisplay) {
-            num1 = num1 * 10;
+            if (floatExp == 0) {
+                num1 = num1 * 10;
+            } else {
+                num1 = num1 * (10 ** floatExp);
+            }
             num1 += button.id - '0';
-            if (floating) {
-                num1 = num1 / 10;
+            if (floatExp > 0) {
+                num1 = num1 / (10 ** floatExp);
+                floatExp++;
             }
         } else if (currOp == null && resultOnDisplay) {
             num1 = button.id - '0';
         } else {
-            num2 = num2 * 10;
+            if (floatExp == 0) {
+                num2 = num2 * 10;
+            } else {
+                num2 = num2 * (10 ** floatExp)
+            }
             num2 += button.id - '0';
-            if (floating) {
-                num2 = num2 / 10;
+            if (floatExp != 0) {
+                num2 = num2 / (10 ** floatExp);
+                floatExp++;
             }
         }
     } else if (button.id == 'x' || button.id == '/' || button.id == '+' || button.id == '-') {
-        if (currOp != null) {
-            operate();
-            num2 = 0;
-        }
         currOp = button.id;
-        floating = false;
+        floatExp = 0;
     } else if (button.id == "del") {
-        if (floating) {
-            floating = false;
+        if (floatExp > 0) {
+            floatExp--;
         }
         if (currOp == null) {
             num1 = Math.floor(num1 / 10);
@@ -47,18 +53,18 @@ function clickButton(button) {
         num1 = 0;
         num2 = 0;
         currOp = null;
-        floating = false;
+        exponent = 0;
     } else if (button.id == '=') {
         operate();
     } else if (button.id == '.') {
-        if (floating) {
+        if (floatExp > 0) {
             return;
         }
         if (resultOnDisplay) {
             num1 = 0;
         }
+        floatExp++;
         isDot = true;
-        floating = true;
     }
     renderDisplay(button.id, isDot);
     if (button.id != '=') {
@@ -80,7 +86,6 @@ function operate() {
     resultOnDisplay = 1;
     num2 = 0;
     currOp = null;
-    floating = false;
 }
 
 function renderDisplay(button, isDot) {
